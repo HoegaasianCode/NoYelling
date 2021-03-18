010abfacda0c6c050c5fb70df4e0bd07b7f87835
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-18 08:18:45 +0100

    Initial commit

---
+++ NoYellingPlease/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NoYellingPlease
+{
+    class Program
+    {
+        // https://edabit.com/challenge/ToMXLjWDLQqFua7Wh
+        // SOLVED
+
+        static void Main(string[] args)
+        {
+            YellDetector yellDetector = new("Skrik inte!!!!!!!!!!"); // "variant"
+            Console.Write(yellDetector.ReverseLoop());
+        }
+    }
+}
---
+++ NoYellingPlease/YellDetector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoYellingPlease
+{
+    public class YellDetector
+    {
+        private readonly string _yell;
+
+        public YellDetector(string yell)
+        {
+            _yell = yell;
+        }
+
+        public string ReverseLoop()
+        {
+            int charCount = 0;
+            for (int i = _yell.Length - 1; i >= 0; i--)
+            {
+                var c = _yell[i];
+                if (c == '!' || c == '?') charCount++;
+            }
+            return _yell.Substring(0, _yell.Length - charCount + 1);
+        }
+    }
+}
